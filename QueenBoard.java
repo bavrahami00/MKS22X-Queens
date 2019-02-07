5d45fb152b830272aaf6fc76c8717c207b60d960
bavrahami00 <bavrahami00@stuy.edu>
2019-02-07 11:35:15 -0500

    Created addQueen and removeQueen methods

--- QueenBoard.java
+++ QueenBoard.java
@@ -3,4 +3,18 @@ public class QueenBoard {
   public QueenBoard(int size) {
     board = new int[size][size];
   }
+  public boolean addQueen(int r, int c) {
+    if (board[r][c] != 0) {
+      return false;
+    }
+    board[r][c] = -1;
+    return true;
+  }
+  public boolean removeQueen(int r, int c) {
+    if (board[r][c] != -1) {
+      return false;
+    }
+    board[r][c] = 0;
+    return true;
+  }
 }
