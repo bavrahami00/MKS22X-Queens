36ecb76c88a3d3aeb481ffac33cd104872a8c0ab
bavrahami00 <bavrahami00@stuy.edu>
2019-02-07 11:38:08 -0500

    Created toString, made private methods private

--- QueenBoard.java
+++ QueenBoard.java
@@ -3,18 +3,31 @@ public class QueenBoard {
   public QueenBoard(int size) {
     board = new int[size][size];
   }
-  public boolean addQueen(int r, int c) {
+  private boolean addQueen(int r, int c) {
     if (board[r][c] != 0) {
       return false;
     }
     board[r][c] = -1;
     return true;
   }
-  public boolean removeQueen(int r, int c) {
+  private boolean removeQueen(int r, int c) {
     if (board[r][c] != -1) {
       return false;
     }
     board[r][c] = 0;
     return true;
+  }
+  public String toString() {
+    for (int x = 0; x < board.length; x++) {
+      for (int i = 0; i < board[0].length; i++) {
+        if (board[x][i] == -1) {
+          System.out.print("Q");
+        }
+        else {
+          System.out.printl("_");
+        }
+      }
+      System.out.println();
+    }
   }
 }
