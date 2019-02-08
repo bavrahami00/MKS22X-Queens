02084c370eb518a7d519729f1c18048f8655584d
bavrahami00 <bavrahami00@stuy.edu>
2019-02-08 14:27:59 -0500

    Partially working spaces

--- QueenBoard.java
+++ QueenBoard.java
@@ -8,6 +8,13 @@ public class QueenBoard {
       return false;
     }
     board[r][c] = -1;
+    for (int x = r+1; x < board.length; x++) {
+      for (int i = 0; i < board[x].length; i++) {
+        if (i-x == c-r) {
+          board[x][i] += 1;
+        }
+      }
+    }
     return true;
   }
   public boolean removeQueen(int r, int c) {
@@ -15,21 +22,47 @@ public class QueenBoard {
       return false;
     }
     board[r][c] = 0;
+    for (int x = r+1; x < board.length; x++) {
+      for (int i = 0; i < board[x].length; i++) {
+        if (i-x == c-r) {
+          board[x][i] -= 1;
+        }
+      }
+    }
     return true;
   }
   public String toString() {
     String ans = "";
     for (int x = 0; x < board.length; x++) {
-      for (int i = 0; i < board[0].length; i++) {
+      for (int i = 0; i < board[x].length; i++) {
         if (board[x][i] == -1) {
-          ans += "Q";
+          ans += "Q ";
+        }
+        else if (board[x][i] > 0) {
+          ans += "# ";
         }
         else {
-          ans += "_";
+          ans += "_ ";
         }
       }
       ans += "\n";
     }
     return ans;
+  }
+  private boolean isEmpty() {
+    for (int x = 0; x < board.length; x++) {
+      for (int i = 0; i < board[x].length; i++) {
+        if (board[x][i] != 0) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  public boolean solve() {
+    if (isEmpty()) {
+      throw new IllegalStateException();
+    }
+    return false;
   }
 }
